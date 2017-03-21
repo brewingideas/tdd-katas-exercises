42d8f2fd457e0dd2831118464f3af3e129a115e5
Liwen Zhang <development@liwen.name>
2017-03-21 20:50:25 +0000

    StringCalculator exercise with Sean, Ellie & Matt #1

--- src/StringCalculator/StringCalculator.js
+++ src/StringCalculator/StringCalculator.js
@@ -1,4 +1,19 @@
 class StringCalculator {
+
+  add(numbers) {
+    const splitNumbers = numbers.split(/[\n,]+/);
+    let sum = 0;
+    if (splitNumbers.length > 1) {
+      for (let i = 0; i < splitNumbers.length; i += 1) {
+        sum += parseInt(splitNumbers[i], 10);
+      }
+      return sum;
+    }
+    if (numbers === '') {
+      return 0;
+    }
+    return 1;
+  }
 }
 
 export default StringCalculator;
